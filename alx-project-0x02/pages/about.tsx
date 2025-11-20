1a0df78a0febef285444e7fd9aabe4c35379a12f
Innosa <innocentokafo@gmail.com>
2025-11-20 22:54:00 +0100

    Update Header with navigation links to /home and /about

---
+++ alx-project-0x02/pages/about.tsx
@@ -0,0 +1,9 @@
+export default function About () {
+    return (
+        <div>
+            <h1>
+                Welcome to about page, alx project routing.
+            </h1>
+        </div>
+    );
+}
---
+++ alx-project-0x02/pages/home.tsx
@@ -0,0 +1,9 @@
+export default function Home () {
+    return (
+        <div>
+            <h1>
+                Welcome to alx project basic routing.
+            </h1>
+        </div>
+    );
+}
